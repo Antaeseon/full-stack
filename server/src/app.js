a4c3a05ede6e856002f7aa9622c96fcd48114eb0
antaeseon <ridgnfu@naver.com>
2018-11-12 02:15:15 +0900

    finish

--- server/src/app.js
+++ server/src/app.js
@@ -3,16 +3,15 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 const morgan = require('morgan')
 
-
 const app = express()
 app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status',(req,res)=> {
-    res.send({
-        message: ' helo world!'
-    })
+app.get('/status', (req, res) => {
+  res.send({
+    message: ' helo world!'
+  })
 })
 
-app.listen(process.env.PORT ||8081)
+app.listen(process.env.PORT || 8081)
